57c1622eb8e7cf3a2f6a2d04f4e4d5f86b5d06e8
gianlucaparadise <gianlucaparadise@gmail.com>
2019-03-14 20:19:19 +0100

    Installed webview; Initial Login flow

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, NativeSyntheticEvent } from 'react-native';
+import { WebView, WebViewNavigation, WebViewError, WebViewIOSLoadRequestEvent } from "react-native-webview";
 
 interface Props { }
 export default class LoginScreen extends Component<Props> {
+	onLoad = (event: NativeSyntheticEvent<WebViewNavigation>) => {
+		console.log(`onLoad`);
+		console.log(event);
+	}
+
+	onLoadStart = (event: NativeSyntheticEvent<WebViewNavigation>) => {
+		console.log(`onLoadStart`);
+		console.log(event);
+	}
+
+	onLoadEnd = (event: NativeSyntheticEvent<WebViewNavigation> | NativeSyntheticEvent<WebViewError>) => {
+		console.log(`onLoadEnd`);
+		console.log(event);
+	}
+
+	onError = (event: NativeSyntheticEvent<WebViewError>) => {
+		console.log(`onError`);
+		console.log(event);
+	}
+
+	onNavigationStateChange = (event: WebViewNavigation) => {
+		console.log(`onNavigationStateChange`);
+		console.log(event);
+	}
+
+	onShouldLoad = (event: WebViewIOSLoadRequestEvent) => {
+		console.log(`onShouldLoad`);
+		console.log(event);
+
+		return true;
+	}
+
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text style={styles.welcome}>Login</Text>
-			</View>
+			<WebView
+				source={{ uri: 'https://albumin-diet-engine.herokuapp.com/auth/spotify' }}
+				onLoad={this.onLoad}
+				onLoadStart={this.onLoadStart}
+				onLoadEnd={this.onLoadEnd}
+				onError={this.onError}
+				onShouldStartLoadWithRequest={this.onShouldLoad}
+				onNavigationStateChange={this.onNavigationStateChange}
+				style={{ marginTop: 20 }}
+			/>
 		);
 	}
 }

605e8128bb7d735a01c41148201ac4d379ca126e
gianlucaparadise <gianlucaparadise@gmail.com>
2019-03-26 07:13:13 +0200

    Revert "React Native Navigation setup"
    This reverts commit 53f71d9a8e0b04674ee1468f1c21e051d763cc56.

--- android/app/src/main/java/com/albumindietmobapp/MainApplication.java
+++ android/app/src/main/java/com/albumindietmobapp/MainApplication.java
@@ -10,43 +10,39 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
-import com.reactnativenavigation.NavigationApplication;
-import com.reactnativenavigation.react.NavigationReactNativeHost;
-import com.reactnativenavigation.react.ReactGateway;
-
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends NavigationApplication {
+public class MainApplication extends Application implements ReactApplication {
 
-	@Override
-	protected ReactGateway createReactGateway() {
-		ReactNativeHost host = new NavigationReactNativeHost(this, isDebug(), createAdditionalReactPackages()) {
-			@Override
-			protected String getJSMainModuleName() {
-				return "index";
-			}
-		};
-		return new ReactGateway(this, isDebug(), host);
-	}
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
 
-	@Override
-	public boolean isDebug() {
-		return BuildConfig.DEBUG;
-	}
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+			    new RNCWebViewPackage()
+      );
+    }
 
-	protected List<ReactPackage> getPackages() {
-		// Add additional packages you require here
-		// No need to add RnnPackage and MainReactPackage
-		return Arrays.<ReactPackage>asList(
-            // the following line is present so that `react-native link` works correctly
-            // new MainReactPackage(),
-			new RNCWebViewPackage()
-		);
-	}
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
 
-	@Override
-	public List<ReactPackage> createAdditionalReactPackages() {
-		return getPackages();
-	}
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
 }
--- index.js
+++ index.js
@@ -3,6 +3,8 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import { start } from "./src/App";
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
-start();
+AppRegistry.registerComponent(appName, () => App);

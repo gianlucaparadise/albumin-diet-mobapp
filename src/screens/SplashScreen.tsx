00618109353937b3d87bf60806d95aed7e666c11
gianlucaparadise <gianlucaparadise@gmail.com>
2019-08-20 16:36:49 +0200

    SplashPage with animation

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image, Animated, Easing, StyleProp, ImageStyle, ViewStyle } from 'react-native';
 import { LoginHelper } from '../helpers/LoginHelper';
 import { NavigationScreenProps } from 'react-navigation';
-import { Text } from 'react-native-paper';
 import { AlbuminColors } from '../Theme';
 
 interface Props extends NavigationScreenProps {
@@ -11,12 +10,14 @@ interface Props extends NavigationScreenProps {
 export default class SplashScreen extends Component<Props> {
 
 	timeout?: number;
+	headphonesAnimatedValue = new Animated.Value(0);
+	fadeAnimatedValue = new Animated.Value(0);
 
 	onPressGo = () => {
 		if (this.timeout) {
 			clearTimeout(this.timeout);
 		}
-		
+
 		this.navigate();
 
 		return true;
@@ -33,16 +34,74 @@ export default class SplashScreen extends Component<Props> {
 		}
 	}
 
-	componentWillMount() {
+	componentDidMount() {
+		this.animate()
+	}
+
+	animate() {
+		this.fadeAnimatedValue.setValue(0);
+		const fadeTiming = Animated.timing(
+			this.fadeAnimatedValue,
+			{
+				toValue: 1,
+				duration: 1000,
+				easing: Easing.linear,
+				useNativeDriver: true,
+			}
+		);
+
+		this.headphonesAnimatedValue.setValue(0);
+		const rotationTiming = Animated.timing(
+			this.headphonesAnimatedValue,
+			{
+				toValue: 1,
+				duration: 300,
+				easing: Easing.exp,
+				useNativeDriver: true,
+			}
+		);
+
+		Animated.sequence([fadeTiming, rotationTiming]).start(() => this.waitAndGo());
+	}
+
+	waitAndGo() {
 		this.timeout = setTimeout(() => {
 			this.onPressGo();
-		}, 1500);
+		}, 700);
 	}
 
 	render() {
+		//#region Headphones animation
+		const rotateX = this.headphonesAnimatedValue.interpolate({
+			inputRange: [0, 1],
+			outputRange: ['90deg', '0deg']
+		});
+
+		const opacity = this.headphonesAnimatedValue.interpolate({
+			inputRange: [0, 0.5, 1],
+			outputRange: [0, 1, 1]
+		});
+
+		// FIXME: rotateX doesn't work on iOS, I don't know why
+		const headphonesStyle = { opacity, transform: [{ rotateX }] };
+		//#endregion
+
+		//#region Fade animation
+		const fade = this.fadeAnimatedValue.interpolate({
+			inputRange: [0, 1],
+			outputRange: [0, 1]
+		});
+
+		const eggStyle = { opacity: fade };
+		//#endregion
+
 		return (
 			<View style={styles.container} onStartShouldSetResponder={() => this.onPressGo()}>
-					<Text style={styles.welcome}>Albumin Diet</Text>
+				<View style={styles.logoContainer}>
+					<Animated.Image resizeMode="contain" style={[styles.logo, eggStyle]} source={require('./../../resources/images/albumin-diet-egg.png')} ></Animated.Image>
+					<Animated.Image resizeMode="contain" style={[styles.logo, headphonesStyle]} source={require('./../../resources/images/albumin-diet-headphones.png')} ></Animated.Image>
+				</View>
+				<Animated.Text style={[styles.welcome, eggStyle]}>Albumin Diet</Animated.Text>
 			</View>
 		);
 	}
@@ -51,13 +110,30 @@ export default class SplashScreen extends Component<Props> {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: 'center',
 		alignItems: 'center',
+		width: '100%',
+		height: '100%',
 		backgroundColor: AlbuminColors.background,
 	},
+	logoContainer: {
+		width: '40%',
+		height: '50%',
+		justifyContent: 'flex-end',
+		position: 'relative',
+		// backgroundColor: 'rgba(0, 0, 100, 0.5)', // this is for debugging
+	},
+	logo: {
+		width: '100%',
+		height: undefined,
+		aspectRatio: 1,
+		position: 'absolute',
+		// backgroundColor: 'rgba(100, 0, 0, 0.5)', // this is for debugging
+	},
 	welcome: {
-		fontSize: 20,
+		fontSize: 30,
 		textAlign: 'center',
 		margin: 10,
-	},
+		color: AlbuminColors.text
+		// backgroundColor: 'rgba(0, 100, 0, 0.5)', // this is for debugging
+	}
 });

1741fce808b9d1980c9b95f9fed2afc90e58952f
gianlucaparadise <gianlucaparadise@gmail.com>
2019-08-29 07:04:35 +0200

    React Native 0.60 migration

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    project: {
+        ios: {},
+        android: {}, // grouped into "project"
+    },
+    assets: [
+        'resources/fonts'
+    ],
+    // commands: require('./path-to-commands.js'), // formerly "plugin", returns an array of commands
+};
